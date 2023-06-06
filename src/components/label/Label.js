aa08bb84b7ef8cc0a06818c139f261673f89684a
Prateek Gupta <pragupta38@WKMZT7CDF889.global.publicisgroupe.net>
2023-06-06 10:47:51 +0530

    fix ui

--- src/components/label/Label.js
+++ src/components/label/Label.js
@@ -8,6 +8,7 @@ const Label = ({ details }) => {
         backgroundColor: `#${details.color}`,
         padding: "4px 14px",
         borderRadius: "8px",
+        height: "fit-content",
       }}>
       {details.name}
     </div>
